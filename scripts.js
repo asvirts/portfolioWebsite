60878f192e561bcb93d434b2b24aade7b1bded54
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2022-07-31 19:50:09 -0500

    Removing some unnecessary JS code

--- scripts.js
+++ scripts.js
@@ -1,18 +1,2 @@
-$(function() {
-    var mt = $('.mainTable'),
-        dv = $('<div/>'),
-        rows = $('tbody tr',mt).clone(),
-        ln = rows.length,
-        ntc = 1;
-    for( var i = 0; i < ln; i = i + 2 ) {
-        var nt = mt.clone().toggleClass('mainTable newTable');
-        $('tbody', nt).html( rows.eq(i).clone() );
-        if( i + 2 <= ln ) {
-            $('tbody',nt).append( rows.eq(i+1).clone() );
-        }
-        dv.append( '<h1>New Table ' + ntc + '</h1>' );
-        dv.append( nt );
-        ntc++;
-    }
-    mt.after( dv.html() );
-});
+let aws = 104
+console.log(aws)
