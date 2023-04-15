79d50364ce4fb2002672a0864c8da42d5861117a
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-04-15 11:43:15 -0500

    still working on it

--- portfolio-website/src/pages/index.js
+++ portfolio-website/src/pages/index.js
@@ -1,14 +1,20 @@
 import { Routes, Route, Link } from "react-router-dom"
-import Nav from '../components/Nav.js'
-import Title from '../components/Title.js'
+import { BrowserRouter } from 'react-router-dom'
+import About from "./About.js"
 
 export default function Home() {
   return (
     <>
-      <nav>
-        <Title />
-        <Nav />
-      </nav>
+      <BrowserRouter>
+        <nav>
+          <Link to="./src/" className="nav-item">Home</Link>
+          <Link to="./src/about" className="nav-item">About</Link>
+        </nav>
+      </BrowserRouter>
+      <Routes>
+        <Route path="./src/" element={<Home />}></Route>
+        <Route path="./src/about" element={<About />}></Route>
+      </Routes>
     </>
   )
 }
