65647c3ac8047e8f77c33d64bbe58f8e819a98c5
Andrew Virts <asvirts@outlook.com>
2023-05-16 17:21:05 -0500

    more styling

--- virts-dev/src/routes/projects.jsx
+++ virts-dev/src/routes/projects.jsx
@@ -10,6 +10,9 @@ export default function Projects() {
             This website is built in React and deployed to Vercel using the
             Vercel CLI.
           </p>
+          <a href="#" class="btn-primary">
+            View Live Site
+          </a>
           <a
             href="https://github.com/asvirts/portfolioWebsite"
             class="btn-primary"
@@ -24,28 +27,46 @@ export default function Projects() {
         <div class="card-body">
           <h3 class="card-title">R&R Window Tinting</h3>
           <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            A website I built for a client that involved SEO work as well as
+            lead generation.
           </p>
           <a
             href="https://rrwindowtinting.com"
-            class="btn btn-primary"
+            class="btn-primary"
             target="_blank"
           >
-            <button>View Site</button>
+            View Live Site
+          </a>
+          <a
+            href="https://github.com/asvirts/r-and-r"
+            class="btn-primary"
+            target="_blank"
+          >
+            View on GitHub
           </a>
         </div>
       </div>
 
       <div class="card">
         <div class="card-body">
-          <h3 class="card-title">Card title</h3>
+          <h3 class="card-title">Lord of the Rings</h3>
           <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Built with React and hosted on Vercel, this site is a hub for all
+            things Lord of the Rings.
           </p>
-          <a href="/" class="btn btn-primary">
-            <button>Go somewhere</button>
+          <a
+            href="https://lotr-lore.vercel.app/"
+            class="btn-primary"
+            target="_blank"
+          >
+            View Live Site
+          </a>
+          <a
+            href="https://github.com/asvirts/lotr-lore"
+            class="btn-primary"
+            target="_blank"
+          >
+            View on GitHub
           </a>
         </div>
       </div>
