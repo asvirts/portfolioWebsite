f9e7a59a936aaca2108e9e4c8b86d699399d1dcd
Andrew Virts <asvirts@outlook.com>
2023-05-20 08:22:02 -0500

    reset

--- virts-dev/src/routes/root.jsx
+++ virts-dev/src/routes/root.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import About from "./about";
 import "../index.css";
@@ -9,10 +9,8 @@ export async function loader() {
 }
 
 export default function Root() {
-  const [theme, setTheme] = useState("light");
-
   return (
-    <div className={theme}>
+    <div>
       <nav className="navbar">
         <div className="logo-sec">
           <img src="./a-logo.png" alt="logo" className="logo" />
@@ -28,15 +26,22 @@ export default function Root() {
           <li>
             <Link to="projects">Projects</Link>
           </li>
-          <button
-            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-          >
-            Current theme: {theme}
-          </button>
         </div>
       </nav>
 
-      <main className="container"></main>
+      <main className="container">
+        <h2>
+          A curious learner and passionate creator sharing my journey with the
+          world.
+        </h2>
+        <p>
+          I have had the opportunity to work on a diverse range of projects,
+          from small business websites to large-scale web applications. I am
+          highly collaborative and enjoy working with a team, but am also
+          comfortable taking on individual projects and seeing them through to
+          completion.
+        </p>
+      </main>
 
       <Outlet />
     </div>
