85686764ab4740ce658c6be98b416e4df1e68531
Andrew Virts <asvirts@outlook.com>
2023-05-29 15:02:18 -0500

    project images

--- virts-dev/src/routes/projects.jsx
+++ virts-dev/src/routes/projects.jsx
@@ -1,20 +1,28 @@
+import Bit16Coder from "../assets/83fc.jpg";
+import Bit16Fellowship from "../assets/fotr16.jpg";
+import Bit16Window from "../assets/16rr.jpg";
+
 export default function Projects() {
   return (
-    <>
+    <div className="container">
       <h2 className="text-center">Projects</h2>
 
       <div class="card">
         <div class="card-body">
-          <div>
+          <img src={Bit16Coder} className="proj" />
+          <div className="w-500 text-left">
             <h3 class="card-title">Virts.dev</h3>
             <p class="card-text">
               This website is built in React and deployed to Vercel using the
               Vercel CLI.
             </p>
           </div>
-          <div className="flex">
-            <a href="#">View Live Site</a>
+          <div>
+            <a className="d-block" href="#">
+              View Live Site
+            </a>
             <a
+              className="d-block"
               href="https://github.com/asvirts/portfolioWebsite"
               target="_blank"
             >
@@ -26,73 +34,113 @@ export default function Projects() {
 
       <div class="card">
         <div class="card-body">
-          <h3 class="card-title">R&R Window Tinting</h3>
-          <p class="card-text">
-            A website I built for a client that involved SEO work as well as
-            lead generation.
-          </p>
-          <div className="flex">
-            <a href="https://rrwindowtinting.com" target="_blank">
-              View Live Site
-            </a>
-            <a href="https://github.com/asvirts/r-and-r" target="_blank">
-              View on GitHub
-            </a>
+          <img src={Bit16Window} className="proj" />
+          <div className="w-500 text-left">
+            <h3 class="card-title">R&R Window Tinting</h3>
+            <p class="card-text">
+              A website I built for a client that involved SEO work as well as
+              lead generation.
+            </p>
           </div>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-body">
-          <h3 class="card-title">Lord of the Rings</h3>
-          <p class="card-text">
-            Built with React, this website explores the lore of J.R.R. Tolkien's
-            Middle-earth.
-          </p>
-          <div className="flex">
-            <a href="https://lotr-lore.vercel.app/" target="_blank">
-              View Live Site
-            </a>
-            <a href="https://github.com/asvirts/lotr-lore" target="_blank">
-              View on GitHub
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-body">
-          <h3 class="card-title">Onyxia's Lair</h3>
-          <p class="card-text">
-            This is a text-based JavaScript game that I developed as a solo
-            project. Can YOU defeat Onyxia?
-          </p>
-          <div className="flex">
-            <a href="https://asvirts.github.io/onyxias-lair" target="_blank">
-              View Live Site
-            </a>
-            <a href="https://github.com/asvirts/onyxias-lair" target="_blank">
-              View on GitHub
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-body">
-          <h3 class="card-title">Password Generator</h3>
-          <p class="card-text">
-            Never use an insecure password again. Automatically generates a
-            secure password.
-          </p>
-          <div className="flex">
+          <div>
             <a
+              className="d-block"
+              href="https://rrwindowtinting.com"
+              target="_blank"
+            >
+              View Live Site
+            </a>
+            <a
+              className="d-block"
+              href="https://github.com/asvirts/r-and-r"
+              target="_blank"
+            >
+              View on GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+          <img src={Bit16Fellowship} className="proj" />
+          <div className="w-500 text-left">
+            {" "}
+            <h3 class="card-title">Lord of the Rings</h3>
+            <p class="card-text">
+              Built with React, this website explores the lore of J.R.R.
+              Tolkien's Middle-earth.
+            </p>
+          </div>
+          <div>
+            <a
+              className="d-block"
+              href="https://lotr-lore.vercel.app/"
+              target="_blank"
+            >
+              View Live Site
+            </a>
+            <a
+              className="d-block"
+              href="https://github.com/asvirts/lotr-lore"
+              target="_blank"
+            >
+              View on GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+          <img src={Bit16Coder} className="proj" />
+          <div className="w-500 text-left">
+            {" "}
+            <h3 class="card-title">Onyxia's Lair</h3>
+            <p class="card-text">
+              This is a text-based JavaScript game that I developed as a solo
+              project. Can YOU defeat Onyxia?
+            </p>
+          </div>
+          <div>
+            <a
+              className="d-block"
+              href="https://asvirts.github.io/onyxias-lair"
+              target="_blank"
+            >
+              View Live Site
+            </a>
+            <a
+              className="d-block"
+              href="https://github.com/asvirts/onyxias-lair"
+              target="_blank"
+            >
+              View on GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+          <img src={Bit16Coder} className="proj" />
+          <div className="w-500 text-left">
+            <h3 class="card-title">Password Generator</h3>
+            <p class="card-text">
+              Never use an insecure password again. Automatically generates a
+              secure password.
+            </p>
+          </div>
+          <div>
+            <a
+              className="d-block"
               href="https://asvirts.github.io/password-generator/"
               target="_blank"
             >
               View Live Site
             </a>
             <a
+              className="d-block"
               href="https://github.com/asvirts/password-generator"
               target="_blank"
             >
@@ -104,19 +152,24 @@ export default function Projects() {
 
       <div class="card">
         <div class="card-body">
-          <h3 class="card-title">React Calculator App</h3>
-          <p class="card-text">
-            A calculator app built with React. This was a fun project that
-            challenged me to think outside the box.
-          </p>
+          <img src={Bit16Coder} className="proj" />
+          <div className="w-500 text-left">
+            <h3 class="card-title">React Calculator App</h3>
+            <p class="card-text">
+              A calculator app built with React. This was a fun project that
+              challenged me to think outside the box.
+            </p>
+          </div>
           <div>
             <a
+              className="d-block"
               href="https://github.com/asvirts/React-Calculator-App"
               target="_blank"
             >
               View Live Site
             </a>
             <a
+              className="d-block"
               href="https://github.com/asvirts/React-Calculator-App"
               target="_blank"
             >
@@ -125,6 +178,6 @@ export default function Projects() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
